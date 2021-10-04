cff5686ae9ca6257941a7d192384d049ddfea671
Seema Neloufer <40924685+seemaneloufer@users.noreply.github.com>
2021-10-05 00:32:16 +0400

    maybe works

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {}
-  properties: any;
-  ngOnInit(): void {
-     this.http
-       .get("data/properties.json")
-       .subscribe((data) => console.log(data));
 
-  }
   title = 'my-first-app';
 }
