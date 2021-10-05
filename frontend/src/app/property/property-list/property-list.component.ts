ee8a65dbaf7328a7b223be35f19bbab7a4e9a11b
Seema Neloufer <40924685+seemaneloufer@users.noreply.github.com>
2021-10-05 06:01:32 +0400

    Modify HTTP Data using Pipe-success

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -1,27 +1,27 @@
 import { HousingService } from './../../services/housing.service';
 
-
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
-properties: any;
+  properties: Array<any>=[];
   constructor(private housingService: HousingService) {
-
+  this.properties = [];
   }
 
   ngOnInit(): void {
-
     this.housingService.getAllProperties().subscribe(
-       data => {
-         this.properties=data;
-         console.log("success");
-       }
+      (data) => {
+        this.properties = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
     );
-      }
-      }
-
+  }
+}
--- frontend/src/app/services/housing.service.ts
+++ frontend/src/app/services/housing.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import {map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +9,18 @@ export class HousingService {
   constructor(private http:HttpClient) { }
   getAllProperties(){
 
-    return this.http.get('assets/data/properties.json')
+    return this.http.get('assets/data/properties.json').pipe(
+      map(
+        data=>{
+          const propertiesArray: Array<any>=[];
+          for (const id in data){
+            if(data.hasOwnProperty(id)){
+            propertiesArray.push(data[id]);
+          }
+        }
+return propertiesArray;
+        }
+      )
+    );
   }
 }
